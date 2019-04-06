f4c28dc959784059bc0a0b278f82d8a62840402c
spmeesseman <spmeesseman@gmail.com>
2019-04-06 16:26:11 -0400

    build(semantic-release): use module itself as changelog generator

---
+++ cyclic.js
@@ -0,0 +1,17 @@
+(function () {
+	var fsUtil = require('fs')
+	var pathUtil = require('path')
+	var existsSync = fsUtil.existsSync || pathUtil.existsSync /* node 0.6 compat */
+	var cwd = __dirname
+	var moduleName = require('./package.json').name
+	var modulePath = pathUtil.join(cwd, 'node_modules', moduleName)
+	var command = 'npm install --force --save-dev '+moduleName
+	var options = {
+		env: process.env,
+		cwd: cwd,
+		stdio: 'inherit' /* stdio doesn't exist below node <0.8 but oh well */
+	}
+	if (existsSync(modulePath) === false ) {
+		require('child_process').exec(command, options)
+	}
+}())
